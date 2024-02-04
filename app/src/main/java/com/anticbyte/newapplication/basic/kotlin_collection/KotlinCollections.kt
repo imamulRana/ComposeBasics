cbebc3a0ba0dad2041bf459c661a82b43047cf94
gh0st <imhasanrana@gmail.com>
2024-02-04 23:36:36 +0600

    Added Kotlin Collections Full

---
+++ app/src/main/java/com/anticbyte/newapplication/basic/kotlin_collection/KotlinCollections.kt
@@ -0,0 +1,60 @@
+package com.anticbyte.newapplication.basic.kotlin_collection
+
+fun main() {
+
+    /*Array*/
+
+    val firstNames = arrayOf("Imamul", "Afredi", "Kawser")
+    val lastNames = arrayOf("Rana", "Rafi", "Raihan")
+
+//    val fullNames = firstNames + lastNames
+//    firstNames[0] = "Hg"
+//    fullNames = arrayOf("Imamul", "Afredi", "Kawser","Rana", "Rafi", "Raihan")
+//    println(fullNames[2])
+
+//    val newArray = firstNames + lastNames
+
+    /*List*/
+
+//    val solarSystem = listOf("Saturn", "Earth", "Moon", "Uranus", "Venus")
+//    for (planet in solarSystem) {
+//        println(planet)
+//    }
+
+    /*Mutable List*/
+
+//    val solarSystem =
+//    mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem)
+//    solarSystem.add("Theia")
+//    solarSystem.removeAt(2)
+//    println("Moon" in solarSystem)
+
+    /*Sets*/
+
+//    val nameSet = linkedSetOf("Rafi", "Rana", "Kawser","Rana")
+//    println("Rafi".hashCode())
+//    println(nameSet)
+
+    /*Map*/
+
+//    data class PersonDetails(
+//        val firstName: String = "",
+//        val lastName: String = "",
+//        val age: Int = 0
+//    )
+//
+//    val ranaDetails = PersonDetails("Imamul", "Rana", 25)
+//    val rafiDetails = PersonDetails("Afredi", "Rafi", 25)
+//
+//
+//    val personDetails = mutableMapOf<String, PersonDetails>(
+//        "Imamul" to ranaDetails,
+//        "Afredi" to rafiDetails
+//    )
+//
+//    val ranaNewDetails = PersonDetails("Imamul Hasan", "Rana", 26)
+//    personDetails["Imamul"] = ranaNewDetails
+//
+//    println(personDetails["Imamul"])
+}
